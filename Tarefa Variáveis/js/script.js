dbbf0e2114732e451836687354b68a230fe81bd6
nathaly <nathalyfroliveira@gmail.com>
2020-07-27 17:28:15 -0300

    codigo da aula

---
+++ Tarefa Variáveis/js/script.js
@@ -0,0 +1,48 @@
+var inteiroPositivo = 10;
+var inteiroNegativo = 7;
+var decimal = 5.5;
+var string = "Nathaly";
+var boolean = true;
+
+
+console.log(inteiroPositivo);
+console.log(inteiroNegativo);
+console.log(decimal);
+console.log(string);
+console.log(boolean);
+
+
+
+var nome = "Nathaly";
+var sobrenome = "Rocha";
+
+console.log(nome);
+console.log(sobrenome);
+console.log(nome + " " + sobrenome);
+
+
+
+console.log(`Tenho 22 anos, comecei estudar programação ha pouco tempo,
+gosto muito de front-end, porém acho que aprender sobre o back-end é essencial`);
+
+
+
+var nulo = null;
+var indefifino;
+
+console.log(nulo);
+console.log(indefifino);
+
+
+
+var frutas = ["Manga", "Maracujá", "Morango", "Melancia", "Melão"];
+
+console.log(frutas);
+
+
+
+var texto2 = (`“Digital House, a escola de programação que forma novas 
+gerações de coders e profissionais digitais para imaginar, 
+inovar  e criar o que sempre sonharam.”`);
+
+console.log(texto2);
